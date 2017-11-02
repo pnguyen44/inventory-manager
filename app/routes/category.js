bc5a215d11554effc70306e15a9a3378c3a910be
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-02 16:10:22 -0400

    Clean code

--- app/routes/category.js
+++ app/routes/category.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecore('category',params.category_id)
+    return this.get('store').findRecore('category',params.category_id);
   }
 });
