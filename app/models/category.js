5670e56abca482921783765dee71ac015f12362c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-02 16:06:12 -0400

    Update category model   - remove {async: true}

--- app/models/category.js
+++ app/models/category.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  items: DS.hasMany('item',{async: true}),
+  items: DS.hasMany('item'),
   isEmpty: Ember.computed('items', function() {
     let items = this.hasMany('items');
     return items.ids().length === 0;
