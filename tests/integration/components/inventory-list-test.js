6b86beebcb9c8b95b8f2f0c802ea495bb37cad3d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-02 11:50:59 -0400

    Add inventory-list and item component   - add a table for content

--- tests/integration/components/inventory-list-test.js
+++ tests/integration/components/inventory-list-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('category-list', 'Integration | Component | category list', {
+moduleForComponent('inventory-list', 'Integration | Component | inventory list', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{category-list}}`);
+  this.render(hbs`{{inventory-list}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#category-list}}
+    {{#inventory-list}}
       template block text
-    {{/category-list}}
+    {{/inventory-list}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
--- tests/integration/components/inventory-list/item-test.js
+++ tests/integration/components/inventory-list/item-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('category-list/item', 'Integration | Component | category list/item', {
+moduleForComponent('inventory-list/item', 'Integration | Component | inventory list/item', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{category-list/item}}`);
+  this.render(hbs`{{inventory-list/item}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#category-list/item}}
+    {{#inventory-list/item}}
       template block text
-    {{/category-list/item}}
+    {{/inventory-list/item}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
