ed3bc93efd81baefc19f200ec902ef5ab74fe4f5
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-02 16:09:05 -0400

    Add createCategory action in category route

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.get('store').findAll('category');
+  },
+  actions: {
+    createCategory(category) {
+      console.log('got here');
+      console.log('category', category);
+      let newCategory = this.get('store').createRecord('category', category);
+      newCategory.save();
+    },
   }
 });
