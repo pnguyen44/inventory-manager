a12f5f487e7f832adbca266f8fe11acb8761bfb0
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-04 21:32:52 -0400

    Refactor code

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
   model() {
     return this.get('store').findAll('category');
   },
+  confirmShown: true,
   actions: {
     createCategory(category) {
       console.log('got here');
@@ -42,37 +43,51 @@ export default Ember.Route.extend({
     createItem(item) {
       console.log('categories route createItem')
       console.log('item is', item)
-      if (item.name !==null) {
+      if (item.name !== null) {
         if (item.name.trim().length) {
           console.log('item in categories route =', item);
           let newItem = this.get('store').createRecord('item', item);
           newItem.save();
-            $(`#item-form-${item.category.id}`).modal('hide');
+          $(`#item-form-${item.category.id}`).modal('hide');
         }
       }
     },
-    editItem (item, updatedItem) {
+    editItem(item, updatedItem) {
       console.log('get to category edit item');
       console.log('item.id', item.id);
-      console.log('updateditem=',updatedItem);
-      let itemName= updatedItem.name;
+      console.log('updateditem=', updatedItem);
+      let itemName = updatedItem.name;
       console.log('itemName', itemName);
 
-      this.get('store').findRecord('item', item.id).then(function (item) {
-        if(updatedItem.name.trim().length){
-          item.set('name',updatedItem.name);
-          item.set('description',updatedItem.description);
-          item.set('currentQuantity',updatedItem.currentQuantity);
-          item.set('alertQuantity',updatedItem.alertQuantity);
+      this.get('store').findRecord('item', item.id).then(function(item) {
+        if (updatedItem.name.trim().length) {
+          item.set('name', updatedItem.name);
+          item.set('description', updatedItem.description);
+          item.set('currentQuantity', updatedItem.currentQuantity);
+          item.set('alertQuantity', updatedItem.alertQuantity);
           item.save();
           $(`#edit-item-form-${item.id}`).modal('hide');
         }
       });
     },
-    deleteItem (item) {
+    deleteItem(item) {
       console.log('got there')
       item.destroyRecord();
       $(`#deleteItemConfirm${item.id}`).modal('hide');
+    },
+    launchConfirmDialog() {
+      console.log('clicked')
+      this.set('confirmShown', true);
+      console.log(this.get('confirmShown'))
+    },
+
+    submitConfirm() {
+      // trigger action on parent component
+      this.set('confirmShown', false);
+    },
+
+    cancelConfirm() {
+      this.set('confirmShown', false);
     },
   }
 });
