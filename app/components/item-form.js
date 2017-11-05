03d3f702cafd7bca57e919d72cd57ea1c4b4409e
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-05 00:38:50 -0400

    Update item-form component   - add message of form   - show message when error occurs

--- app/components/item-form.js
+++ app/components/item-form.js
@@ -1,33 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-newItem: {
-  name:null,
-  description: null,
-  currentQuantity: null,
-  alertQuantity: null,
-},
-actions: {
-  createItem (){
-    console.log('in item-form');
-    console.log('newItem..', this.get('newItem'));
-    console.log('category..', this.get('category'));
-    let newItemName = this.get('newItem.name')
-    if (newItemName !== null) {
-      if (newItemName.trim().length){
+  newItem: {
+    name: null,
+    description: null,
+    currentQuantity: null,
+    alertQuantity: null,
+  },
+  actions: {
+    createItem() {
+      if (this.get('newItem.name') !== undefined) {
+        console.log('newItem lenght is', this.get('newItem.name').length)
+        this.set('message', 'Name Required')
+        Ember.$('.message').show()
+        Ember.$('.message').delay(1000).fadeOut('slow')
+      }
+      if (this.get('newItem.name') === undefined) {
+        console.log('newItem name is in erro', this.get('newItem.name'))
+        this.set('message', 'Name Required')
+
+        Ember.$('.message').show()
+        Ember.$('.message').delay(1000).fadeOut('slow')
         this.get('newItem').category = this.get('category');
         console.log('category in if', this.get('category'));
+
       }
+      // let itemObj = this.get('newItem')
+      // itemObj.category= this.get('category')
+      // console.log('----', newItem)
+      this.sendAction('createItem', this.get('newItem'));
+      this.set('newItem', {});
+      console.log('newItem after reset', this.get('newItem'))
+    },
+    clearItemForm() {
+      this.set('newItem', {});
     }
-    // let itemObj = this.get('newItem')
-    // itemObj.category= this.get('category')
-    // console.log('----', newItem)
-    this.sendAction('createItem', this.get('newItem'));
-    this.set('newItem',{});
-    console.log('newItem after reset', this.get('newItem'))
-  },
-  clearItemForm () {
-    this.set('newItem',{});
   }
-}
 });
