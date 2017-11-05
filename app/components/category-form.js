131402e26aeb0ebadcb63e9ca4f0d815a556cfc7
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-04 23:50:30 -0400

    Add error handler to category-form   - update form to include message when there is an error

--- app/components/category-form.js
+++ app/components/category-form.js
@@ -5,13 +5,20 @@ export default Ember.Component.extend({
     name: null,
   },
   actions: {
-    createCategory () {
+    createCategory() {
       console.log('got here...');
       console.log('tite=', this.get('title'));
-      console.log('new category =', this.get('newCategoryToAdd'));
-      // console.log('form submitted')
-      this.sendAction('createCategory',this.get('newCategoryToAdd'));
-      this.set('newCategoryToAdd.name',null);
+      console.log('new category =', this.get('newCategoryToAdd.name'));
+      if (this.get('newCategoryToAdd.name') === null){
+          console.log('error ..')
+          this.set('message', 'Error')
+          Ember.$('.message').delay(1000).fadeOut('slow')
+      } else if (this.get('newCategoryToAdd.name').trim().length = 0) {
+        this.set('message', 'Name Required')
+        Ember.$('.message').delay(1000).fadeOut('slow')
+      }
+      this.sendAction('createCategory', this.get('newCategoryToAdd'));
+      this.set('newCategoryToAdd.name', null);
     }
   }
 });
