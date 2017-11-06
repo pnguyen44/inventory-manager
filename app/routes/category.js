05c3711c619e16024fe438de039b52db467d4fed
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-06 11:32:49 -0500

    Update actions on category controller   - add error handling for actions create, delete, and update for items   and category

--- app/routes/category.js
+++ app/routes/category.js
@@ -9,44 +9,66 @@ export default Ember.Route.extend({
     deleteCategory(category) {
       console.log('deleting')
       category.destroyRecord()
-      .then(() => {
-        $(`#deleteCategoryConfirm${category.id}`).modal('hide');
-        this.transitionTo('categories');
-      })
+        .then(() => {
+          Ember.$(".modal-backdrop").remove()
+          Ember.$(`#deleteCategoryConfirm${category.id}`).modal('hide');
+          this.transitionTo('categories');
+        })
     },
     createItem(item) {
       console.log('categories route createItem')
       console.log('item is', item)
-      if (item.name !== null && item.name !==undefined) {
+      if (item.name !== null && item.name !== undefined) {
         if (item.name.trim().length) {
           console.log('item in categories route =', item);
           let newItem = this.get('store').createRecord('item', item);
-          newItem.save();
-          $(`#item-form-${item.category.id}`).modal('hide');
+          newItem.save()
+            .then(() => {
+              Ember.$(".modal-backdrop").remove()
+              Ember.$(`#item-form-${item.category.id}`).modal('hide');
+            })
+            .catch(() => {
+              newItem.rollbackAttributes()
+              Ember.$('.message').show()
+              Ember.$('.message').html('Error on create item')
+              Ember.$('.message').delay(1300).fadeOut('slow')
+            })
         }
       }
     },
-    editItem (item, updatedItem) {
+    editItem(item, updatedItem) {
       console.log('get to category edit item');
       console.log('item.id', item.id);
-      console.log('updateditem=',updatedItem);
-      let itemName= updatedItem.name;
+      console.log('updateditem=', updatedItem);
+      let itemName = updatedItem.name;
       console.log('itemName', itemName);
 
-      this.get('store').findRecord('item', item.id).then(function (item) {
-        if(updatedItem.name.trim().length){
-          item.set('name',updatedItem.name);
-          item.set('description',updatedItem.description);
-          item.set('currentQuantity',updatedItem.currentQuantity);
-          item.set('alertQuantity',updatedItem.alertQuantity);
-          item.save();
-          $(`#edit-item-form-${item.id}`).modal('hide');
+      this.get('store').findRecord('item', item.id).then(function(item) {
+        if (updatedItem.name.trim().length) {
+          item.set('name', updatedItem.name);
+          item.set('description', updatedItem.description);
+          item.set('currentQuantity', updatedItem.currentQuantity);
+          item.set('alertQuantity', updatedItem.alertQuantity);
+          item.save()
+            .then(() => {
+              Ember.$(".modal-backdrop").remove()
+              Ember.$(`#edit-item-form-${item.id}`).modal('hide');
+            })
+            .catch(() => {
+              item.rollbackAttributes()
+              Ember.$('.message').show()
+              Ember.$('.message').html('Error on edit item')
+              Ember.$('.message').delay(1300).fadeOut('slow')
+            })
         }
       });
     },
-    deleteItem (item) {
-      item.destroyRecord();
-      $(`#deleteItemConfirm${item.id}`).modal('hide');
+    deleteItem(item) {
+      item.destroyRecord()
+        .then(() => {
+          Ember.$(".modal-backdrop").remove()
+          Ember.$(`#deleteItemConfirm${item.id}`).modal('hide');
+        })
     }
   }
 });
