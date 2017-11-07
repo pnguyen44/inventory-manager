cd71e4e41b47685d80acc9ef19aabf874b3195cf
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-06 22:47:55 -0500

    Update edit and add item action   -convert current quantity, alert quantity, and purchasedQuantity to a   number type

--- app/routes/category.js
+++ app/routes/category.js
@@ -21,6 +21,9 @@ export default Ember.Route.extend({
       console.log('item currentQuantity--', item.currentQuantity)
       if (item.name.trim().length && item.currentQuantity !== undefined && item.currentQuantity !== null && item.currentQuantity !== '') {
           console.log('item in categories route =', item);
+          item.currentQuantity=Number(item.currentQuantity)
+          item.alertQuantity=Number(item.alertQuantity)
+          item.quantityPurchased=0
           let newItem = this.get('store').createRecord('item', item);
           newItem.save()
             .then(() => {
@@ -31,12 +34,12 @@ export default Ember.Route.extend({
               newItem.rollbackAttributes()
               Ember.$('.message').show()
               Ember.$('.message').html('Error on create item')
-              Ember.$('.message').delay(1300).fadeOut('slow')
+              Ember.$('.message').delay(1500).fadeOut('slow')
             })
         } else {
           Ember.$('.message').show()
           Ember.$('.message').html('Name and current quantity required')
-          Ember.$('.message').delay(1400).fadeOut('slow')
+          Ember.$('.message').delay(1500).fadeOut('slow')
         }
       // }
     },
@@ -54,8 +57,8 @@ export default Ember.Route.extend({
         if (updatedItem.name.trim().length && updatedItem.currentQuantity !== undefined && updatedItem.currentQuantity !== null && updatedItem.currentQuantity !== '') {
           item.set('name', updatedItem.name);
           item.set('description', updatedItem.description);
-          item.set('currentQuantity', updatedItem.currentQuantity);
-          item.set('alertQuantity', updatedItem.alertQuantity);
+          item.set('currentQuantity', Number(updatedItem.currentQuantity));
+          item.set('alertQuantity', Number(updatedItem.alertQuantity));
           item.save()
             .then(() => {
               Ember.$(".modal-backdrop").remove()
@@ -65,12 +68,12 @@ export default Ember.Route.extend({
               item.rollbackAttributes()
               Ember.$('.message').show()
               Ember.$('.message').html('Error on edit item')
-              Ember.$('.message').delay(1400).fadeOut('slow')
+              Ember.$('.message').delay(1500).fadeOut('slow')
             })
         } else {
           Ember.$('.message').show()
           Ember.$('.message').html('Name and current quantity required')
-          Ember.$('.message').delay(1400).fadeOut('slow')
+          Ember.$('.message').delay(1500).fadeOut('slow')
         }
       });
     },
