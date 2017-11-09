cc98f52d98ceb244de6d2906d52f89ba6e59e34c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-09 16:59:51 -0500

    Add header-button component and index route   - add  button in header which will be get started if the user has not   signed in and view inventory if the user has signed in

---
+++ app/components/header-button.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  auth: Ember.inject.service(),
+
+user: Ember.computed.alias('auth.credentials.email'),
+isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+text: Ember.computed('isAuthenticated', function () {
+  if(this.get('isAuthenticated')) {
+    return 'View Inventory'
+  } else {
+    return 'Get Started'
+  }
+})
+});
