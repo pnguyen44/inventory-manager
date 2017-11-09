e80101530dcd29baa5b7eb075240558b1d7fe585
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-09 17:10:31 -0500

    Refactor code

--- app/components/header-button.js
+++ app/components/header-button.js
@@ -2,14 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   auth: Ember.inject.service(),
-
-user: Ember.computed.alias('auth.credentials.email'),
-isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
-text: Ember.computed('isAuthenticated', function () {
-  if(this.get('isAuthenticated')) {
-    return 'View Inventory'
-  } else {
-    return 'Get Started'
-  }
-})
+  user: Ember.computed.alias('auth.credentials.email'),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  text: Ember.computed('isAuthenticated', function() {
+    if (this.get('isAuthenticated')) {
+      return 'View Inventory'
+    } else {
+      return 'Get Started'
+    }
+  }),
+  // linkToText: Ember.computed('isAuthenticated', function() {
+  //   if (this.get('isAuthenticated')) {
+  //     return 'category'
+  //   } else {
+  //     return 'signed-up'
+  //   }
+  // })
 });
